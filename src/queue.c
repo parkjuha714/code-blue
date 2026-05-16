9d9d937b847db3584034124600e0b79942bec8db
parkjuha714 <20250714@gs.anyang.ac.kr>
2026-05-16 20:46:44 +0900

    feat: add queue.c skeleton with TODO comments

---
+++ src/queue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../include/queue.h"
+
+/*
+ * 원형 큐 (Circular Queue) 구현
+ * 일반 대기 환자를 선착순(FIFO)으로 관리
+ *
+ * TODO: enqueue, dequeue, is_empty, is_full 구현 예정
+ */
+
+void queue_placeholder(void) {
+    // 다음 commit에서 실제 구현 예정
+}
